473162507220089a33af556cae4fc214b76b3bfd
Anshu <anshu.possible@gmail.com>
2024-02-21 23:26:16 +0530

    added mock data from utils and filter it

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,125 +1,12 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
 import { useState } from "react";
+import resList from "../../utils/mockData";
 
 const Body = () => {
   //Local state Variable
 
-  const [ListOfRestaurant, setListOfRestaurant] = useState([
-    {
-      data: {
-        id: "334475",
-        name: "KFC",
-        cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-        cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-        costForTwo: 40000,
-        costForTwoString: "₹400 FOR TWO",
-        deliveryTime: 36,
-        minDeliveryTime: 36,
-        avgRating: "3.8",
-      },
-    },
-    {
-      data: {
-        id: "334476",
-        name: "Meghana Foods",
-        cloudinaryImageId: "xqwpuhgnsaf18te7zvtv",
-        cuisines: [
-          "Biryani",
-          "Andhra",
-          "South Indian",
-          "North Indian",
-          "Chinese",
-          "Seafood",
-        ],
-        costForTwo: 40000,
-        costForTwoString: "₹400 FOR TWO",
-        deliveryTime: 36,
-        minDeliveryTime: 36,
-        avgRating: "4.5",
-      },
-    },
-
-    {
-      data: {
-        id: "334478",
-        name: "Spices",
-        cloudinaryImageId: "xqwpuhgnsaf18te7zvtv",
-        cuisines: [
-          "Biryani",
-          "Andhra",
-          "South Indian",
-          "North Indian",
-          "Chinese",
-          "Seafood",
-        ],
-        costForTwo: 40000,
-        costForTwoString: "₹400 FOR TWO",
-        deliveryTime: 36,
-        minDeliveryTime: 36,
-        avgRating: "4.5",
-      },
-    },
-  ]);
-
-  //Normal JS variable
-  // let ListOfRestaurantJS = [
-  //   {
-  //     data: {
-  //       id: "334475",
-  //       name: "KFC",
-  //       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-  //       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-  //       costForTwo: 40000,
-  //       costForTwoString: "₹400 FOR TWO",
-  //       deliveryTime: 36,
-  //       minDeliveryTime: 36,
-  //       avgRating: "3.8",
-  //     },
-  //   },
-
-  //   {
-  //     data: {
-  //       id: "334476",
-  //       name: "Meghana Foods",
-  //       cloudinaryImageId: "xqwpuhgnsaf18te7zvtv",
-  //       cuisines: [
-  //         "Biryani",
-  //         "Andhra",
-  //         "South Indian",
-  //         "North Indian",
-  //         "Chinese",
-  //         "Seafood",
-  //       ],
-  //       costForTwo: 40000,
-  //       costForTwoString: "₹400 FOR TWO",
-  //       deliveryTime: 36,
-  //       minDeliveryTime: 36,
-  //       avgRating: "4.5",
-  //     },
-  //   },
-
-  //   {
-  //     data: {
-  //       id: "334478",
-  //       name: "Spices",
-  //       cloudinaryImageId: "xqwpuhgnsaf18te7zvtv",
-  //       cuisines: [
-  //         "Biryani",
-  //         "Andhra",
-  //         "South Indian",
-  //         "North Indian",
-  //         "Chinese",
-  //         "Seafood",
-  //       ],
-  //       costForTwo: 40000,
-  //       costForTwoString: "₹400 FOR TWO",
-  //       deliveryTime: 36,
-  //       minDeliveryTime: 36,
-  //       avgRating: "4.5",
-  //     },
-  //   },
-  // ];
+  const [ListOfRestaurant, setListOfRestaurant] = useState(resList);
 
   return (
     <div className="body">
@@ -128,11 +15,11 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             // filter logic here
-            
+
             const filteredList = ListOfRestaurant.filter(
               (res) => res.data.avgRating > 4
             );
-           setListOfRestaurant(filteredList);
+            setListOfRestaurant(filteredList);
           }}
         >
           Top Rated Restaurant
